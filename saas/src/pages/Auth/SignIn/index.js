fee8342c253ebae28eefbc8ba11389ac428ca460
Davi Holanda <daviht7@gmail.com>
2020-01-01 14:33:08 -0300

    finalizando tela de login estilização

--- saas/src/pages/Auth/SignIn/index.js
+++ saas/src/pages/Auth/SignIn/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container,SignInForm } from '../styles';
+import Button from '~/styles/components/Button'
 
 export default function SignIn() {
   return (
-    <div />
+    <Container>
+      <SignInForm onSubmit={() => {}}>
+        <h1>Boas Vindas</h1>
+
+        <label htmlFor="email">E-mail</label>
+        <input type="email" id="email" name="email" />
+
+        <label htmlFor="password">Senha</label>
+        <input type="password" id="password" name="password" />
+
+        <Button size="big" type="submit">Entrar</Button>
+      </SignInForm>
+    </Container>
   );
 }
