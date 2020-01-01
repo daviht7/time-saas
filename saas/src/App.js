0983fd5e3568c4be5a591ea9563aa5fd9c0bf02c
Davi Holanda <daviht7@gmail.com>
2020-01-01 13:54:22 -0300

    configurando estilo global e store e saga

--- saas/src/App.js
+++ saas/src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Provider } from "react-redux";
+
 import Routes from "~/routes";
+import store from '~/store'
+
+import GlobalStyle from '~/styles/global'
 
 function App() {
-  return <Routes />
+  return (
+    <Provider store={store}>
+      <Fragment>
+        <Routes />
+        <GlobalStyle />
+      </Fragment>
+    </Provider>
+  )
 }
 
 export default App;
